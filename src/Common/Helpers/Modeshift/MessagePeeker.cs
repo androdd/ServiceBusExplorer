371ea0ceac678a824829b021026fab462b6fe9de
Andrey Dimitrov <andrey.dimitrov@modeshift.com>
2024-06-09 21:44:38 +0300

    Slight performance improvement

--- src/Common/Helpers/Modeshift/MessagePeeker.cs
+++ src/Common/Helpers/Modeshift/MessagePeeker.cs
@@ -24,9 +24,17 @@ namespace ServiceBusExplorer.Common.Helpers.Modeshift
             this.isPartitionedTopic = isPartitionedTopic;
         }
 
-        public List<BrokeredMessage> Peek(
-            int count,
-            long deadLetterMessageCount)
+        public List<BrokeredMessage> Peek(int count, long deadLetterMessageCount)
+        {
+            if(isPartitionedTopic)
+            {
+                return PeekPartitioned(count, deadLetterMessageCount);
+            }
+
+            return PeekUnpartitioned(count, deadLetterMessageCount); 
+        }
+
+        private List<BrokeredMessage> PeekUnpartitioned(int count, long deadLetterMessageCount)
         {
             var peeks = 0;
 
@@ -56,6 +64,65 @@ namespace ServiceBusExplorer.Common.Helpers.Modeshift
                 messages = PeekBatch(lowerBound + step, count).ToList();
                 peeks += isPartitionedTopic ? messages.Count() : 1;
                 if (messages.Count < count)
+                {
+                    break;
+                }
+                lowerBound += step;
+                step *= 2;
+            }
+
+            // Binary search to find the approximate last position
+            var upperBound = lowerBound + step;
+            while (lowerBound < upperBound)
+            {
+                var midPoint = (lowerBound + upperBound) / 2;
+                messages = PeekBatch(midPoint, count).ToList();
+                peeks += isPartitionedTopic ? messages.Count() : 1;
+                if (messages.Count < count)
+                {
+                    upperBound = midPoint;
+                }
+                else
+                {
+                    lowerBound = midPoint + 1;
+                }
+            }
+
+            writeToLog($"Peeked with {peeks} api calls.");
+
+            return messages;
+        }
+
+        public List<BrokeredMessage> PeekPartitioned(int count, long deadLetterMessageCount)
+        {
+            var peeks = 0;
+            var messages = new List<BrokeredMessage>();
+
+            if (deadLetterMessageCount < 1)
+            {
+                return messages;
+            }
+
+            if(deadLetterMessageCount <= count)
+            {
+                messages = PeekBatch(count);
+                writeToLog($"Peeked with {messages.Count} api calls.");
+                return messages.ToList();
+            }
+
+            var initialMessages = PeekBatch(1);
+            peeks++;
+                        
+            var initialSequence = initialMessages.Last().SequenceNumber;
+            var lowerBound = initialSequence;
+            var step = deadLetterMessageCount;
+            
+            // Incrementally find an upper bound
+            while (true)
+            {
+                messages = PeekBatch(lowerBound + step, 1).ToList();
+                peeks++;
+                if (!messages.Any())
                 {
                     break;
                 }
